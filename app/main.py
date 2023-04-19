3d83582f9cb644a29606dd9e8dbc6d088a1dd333
m1raynee <pechenko-7@ya.ru>
2023-04-19 18:21:22 +0300

    apply group router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
 from .routers import router
 
 app = FastAPI()
+add_pagination(app)
 app.include_router(router)
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,7 @@
-from . import students
+from . import students, groups
 
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(students.router)
+router.include_router(groups.router)
